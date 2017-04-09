fbe591d69ab3f788b40efdb180059b4483b58705
crusadetx <angel.terziev97@gmail.com>
2017-04-09 11:52:11 +0300

    Commit to branch b1

--- Maven2/src/main/java/com/fmi/maven/Maven2/App.java
+++ Maven2/src/main/java/com/fmi/maven/Maven2/App.java
@@ -8,6 +8,6 @@ public class App
 {//kjkjuhji
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello!" );
     }//654646
 }
